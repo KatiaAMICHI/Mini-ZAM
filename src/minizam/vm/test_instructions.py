f5ca883587cb890fbb836581da20ad204833abf9
Nadir Belarouci <nadir.belarrouci@gmail.com>
2019-03-07 01:34:40 +0100

    TODO: BranchTest and BranchfNotTest

--- src/minizam/vm/test_instructions.py
+++ src/minizam/vm/test_instructions.py
@@ -31,18 +31,32 @@ class PrimTest(unittest.TestCase):
         self.vm.push([MLValue.from_int(5), MLValue.from_int(2), MLValue.from_int(4)])
         self.vm.set_accumulator(MLValue.from_int(10))
 
-    def test_add(self):
-        self.vm.current_args = ["+"]
+    def do_operation(self, op, result):
+        self.vm.current_args = [op]
         MiniZamVM.instructions["PRIM"].execute(self.vm)
-        self.assertEqual(MLValue.from_int(15), self.vm.get_accumulator())
+        self.assertEqual(MLValue.from_int(result), self.vm.get_accumulator())
         self.assertEqual(self.vm.stack.size(), 2)
         self.assertEqual(self.vm.peek(), MLValue.from_int(2))
+
+    def test_add_minus_mul_div(self):
+        self.do_operation("+", 15)
+        self.do_operation("-", 5)
+        self.do_operation("*", 150)
+        self.do_operation("/", 2)
 
         # TODO check when in acc there is a closure and expect error typeError / or in Stack
 
-    def test_minus(self):
-        self.vm.current_args = ["-"]
-        MiniZamVM.instructions["PRIM"].execute(self.vm)
-        self.assertEqual(MLValue.from_int(5), self.vm.get_accumulator())
-        self.assertEqual(self.vm.stack.size(), 2)
-        self.assertEqual(self.vm.peek(), MLValue.from_int(2))
+    def test_or_and(self):
+        pass
+
+    def test_notEq_eq_lt_gt_ltEq_gtEq(self):
+        pass
+
+
+class BranchTest(unittest.TestCase):
+    pass
+
+
+class BranchIfNotTest(unittest.TestCase):
+    pass
+
